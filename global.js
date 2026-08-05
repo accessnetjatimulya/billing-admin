18c92a5a1bbe75114e4635a4b1d65c78403e2f8c
accessnetjatimulya <accessnetjatimulya@gmail.com>
2026-08-05 11:38:35 +0700

    Add files via upload

--- global.js
+++ global.js
@@ -1,7 +1,7 @@
 // ==========================================
 // CONFIG: PASTE URL WEB APP APPS SCRIPT DI SINI
 // ==========================================
-const URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycbx9KL7PP4wHk15VL3WcW-kNNfMIjEhIHVUHaTOg7Cs5PdtPlyBJxI4zx5U5u5tDRBk/exec";
+const URL_APPS_SCRIPT = "https://script.google.com/macros/s/AKfycby-MonnsgUOPgwFUHNs0gjuWIV5bQP94P7vxBlYH5-2Fc_5q6NoTgP1SERjK3a9tHky/exec";
 
 // Fungsi Global Jembatan API
 function callAPI(action, data, onSuccess, onFailure) {
